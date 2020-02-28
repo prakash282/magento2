be959e5f30ecfd41839a8954f1b6e365b3da0d3e
prakash282 <paks.r.patel@gmail.com>
2020-02-28 18:45:46 +0530

    Update registration.php

--- app/code/Magento/CatalogRule/registration.php
+++ app/code/Magento/CatalogRule/registration.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Magento_CatalogRule',
